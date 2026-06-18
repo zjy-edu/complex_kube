a3a64cc45679a4b7fd4e77e7e5fa46020165b91e
Zelalem Jembre Yalew <zizutg@gmail.com>
2026-06-18 15:40:32 -0400

    kubernetes completed

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const pgClient = new Pool({
   password: keys.pgPassword,
   port: keys.pgPort,
   ssl:
-    process.env.NODE_ENV !== 'production'
+    process.env.NODE_ENV !== 'production' || process.env.PGSSLMODE === 'disable'
       ? false
       : { rejectUnauthorized: false },
 });
